bf40b0a9d1bcec2da67fd949f21a8e2aa1f9cc15
Denys Lygach <denisligach98@gmail.com>
2024-08-05 22:32:49 +0300

    storefront

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\StoreFront;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', StoreFront::class)->name('home');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+//Route::middleware([
+//    'auth:sanctum',
+//    config('jetstream.auth_session'),
+//    'verified',
+//])->group(function () {
+//    Route::get('/dashboard', function () {
+//        return view('dashboard');
+//    })->name('dashboard');
+//});
