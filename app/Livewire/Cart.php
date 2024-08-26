29d11afb8029d4ec3fe98c9230ff0cb049cc6444
Denys Lygach <denisligach98@gmail.com>
2024-08-26 10:39:14 +0300

    cart

--- app/Livewire/Cart.php
+++ app/Livewire/Cart.php
@@ -9,21 +9,26 @@ use Livewire\Component;
 
 class Cart extends Component
 {
+    public function getCartProperty()
+    {
+        return CartFactory::make()->loadMissing(['items', 'items.product', 'items.variant']);
+    }
+
     public function getItemsProperty()
     {
-        return CartFactory::make()->items()->with('variant', 'product')->get();
+        return $this->cart->items()->with('variant', 'product')->get();
     }
 
     public function increment($itemId)
     {
-        CartFactory::make()->items()->find($itemId)->increment('quantity');
+        $this->cart->items->find($itemId)->increment('quantity');
 
         $this->dispatch('productAddedToCart');
     }
 
     public function decrement($itemId)
     {
-        $item = CartFactory::make()->items()->find($itemId);
+        $item = $this->cart->items->find($itemId);
 
         if ($item->quantity > 1) {
             $item->decrement('quantity');
@@ -38,7 +43,7 @@ class Cart extends Component
 
     public function delete($itemId)
     {
-        CartFactory::make()->items()->where('id', $itemId)->delete();
+        $this->cart->where('id', $itemId)->delete();
 
         $this->dispatch('productRemovedFromCart');
     }
--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -1,39 +1,71 @@
-<div class="mt-12 p-5 bg-white rounded-lg shadow">
-    <table class="w-full">
-        <thead>
+<div class="grid grid-cols-4 mt-12 gap-4">
+    <div class="col-span-3 p-5 bg-white rounded-lg shadow">
+        <table class="w-full">
+            <thead>
             <tr>
                 <th class="text-left">Product</th>
+                <th class="text-left">Price</th>
+                <th class="text-left">Color</th>
+                <th class="text-left">Size</th>
                 <th class="text-left">Quantity</th>
+                <th class="text-left">Total</th>
+                <th></th>
             </tr>
-        </thead>
-        <tbody>
-        @foreach($this->items as $item)
-            <tr wire:key="$item->id">
-                <td>{{ $item->product->name }} Size: {{ $item->variant->size }} Color: {{ $item->variant->color }}</td>
-                <td class="flex items-center gap-4">
-                    <button wire:click="decrement({{ $item->id }})">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
-                        </svg>
-                    </button>
-                    <div>
-                        {{ $item->quantity }}
-                    </div>
-                    <button wire:click="increment({{ $item->id }})">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                        </svg>
-                    </button>
-                </td>
-                <td>
-                    <button wire:click="delete({{ $item->id }})">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                        </svg>
-                    </button>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            @foreach($this->items as $item)
+                <tr wire:key="$item->id">
+                    <td>{{ $item->product->name }}</td>
+                    <td>{{ $item->product->price }}</td>
+                    <td>{{ $item->variant->color }}</td>
+                    <td>{{ $item->variant->size }}</td>
+                    <td class="flex items-center gap-4">
+                        <button wire:click="decrement({{ $item->id }})">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+                            </svg>
+                        </button>
+                        <div>
+                            {{ $item->quantity }}
+                        </div>
+                        <button wire:click="increment({{ $item->id }})">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            </svg>
+                        </button>
+                    </td>
+                    <td>{{ $item->subtotal }}</td>
+                    <td>
+                        <button wire:click="delete({{ $item->id }})">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            </svg>
+                        </button>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td class="text-right font-medium" colspan="5">Total:</td>
+                    <td class="font-medium">{{ $this->cart->total }}</td>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+    <div>
+        <div class="p-5 bg-white rounded-lg shadow col-span-1">
+            @guest
+                <p>Please <a class="underline" href="{{ route('register') }}">register</a> or <a class="underline"
+                                                                                                 href="{{ route('login') }}">login</a>
+                    to continue.</p>
+            @endguest
+
+            @auth
+                <div class="flex justify-center items-center h-full">
+                    <x-button wire:click="checkOut">Checkout</x-button>
+                </div>
+            @endauth
+        </div>
+    </div>
 </div>
